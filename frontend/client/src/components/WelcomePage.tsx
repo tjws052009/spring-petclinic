465a7b1f4361fa34450b3ffd700b623ab9574bff
adamquan <adam.quan@elastic.co>
2019-03-18 23:48:57 -0500

    Removed custom instrumentation

--- frontend/client/src/components/WelcomePage.tsx
+++ frontend/client/src/components/WelcomePage.tsx
@@ -1,24 +1,9 @@
 import * as React from 'react';
-import { APMService, punish } from '../main';
 export default class WelcomePage extends React.Component<any, any> {
 
   constructor() {
     super();
   }
-
-  componentWillMount() {
-    APMService.getInstance().startTransaction('WelcomePage');
-    punish();
-  }
-
-  componentDidMount() {
-    APMService.getInstance().endTransaction(true);
-  }
-
-  componentWillUnmount() {
-    APMService.getInstance().endTransaction(false);
-  }
-
 
   render() {
     return (
