5e297a8eb1c58f42e3777d5bb14b02575d5ef5bf
Adam Quan <adam.quan@gmail.com>
2019-04-17 18:38:19 -0500

    Update setAPMenv.sh

--- bin/setAPMenv.sh
+++ bin/setAPMenv.sh
@@ -2,14 +2,14 @@
 export ELASTIC_APM_SERVER_URL=
 
 # This is the secrete token of your APM server
-ELASTIC_APM_SECRET_TOKEN=
+export ELASTIC_APM_SECRET_TOKEN=
 
 # This is your Elassticsearch server URL
 # This is for the cluster that stores the address data
-ELASTICSEARCH_URL=
+export ELASTICSEARCH_URL=
 
 # This is your Elassticsearch password for user "elastic" 
-ELASTICSEARCH_PASSWORD=
+export ELASTICSEARCH_PASSWORD=
 
 
 
